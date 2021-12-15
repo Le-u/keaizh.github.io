8220cce82a80a71a6119af8d2f9d7bd9a829a72e
A-Little-Reunion <370047940@qq.com>
2021-12-15 15:16:21 +0800

    多次弹出重叠问题

--- component/notify/notify.js
+++ component/notify/notify.js
@@ -1,7 +1,16 @@
-var notifyFun = function(txt,obj,time = 2){
-  var notify = $(`<div class="notify"> <span class="glass notify-text">${txt}</span> </div>`).css({...obj}).appendTo('body');
-  
-  setTimeout(()=>{
-    notify.hide();
-  },time * 1000)
+var retTime = null;
+var notifyFun = function (txt, obj, time = 2) {
+
+  if (!retTime) {
+    var notify = $(`<div class="notify"> <span class="glass notify-text">${txt}</span> </div>`).css({
+      ...obj
+    }).appendTo('body');
+
+    retTime = setTimeout(() => {
+      notify.remove();
+      retTime = null
+    }, time * 1000)
+  }
+
+ 
 }
