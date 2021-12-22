7e498414b3b7fb9804be57f39ee5cb51efada7da
A-Little-Reunion <370047940@qq.com>
2021-12-22 17:13:27 +0800

    更新

---
+++ component/maxPhoto/maxPhoto.js
@@ -0,0 +1,50 @@
+var maxPhoto = function (i) {
+  urlIndex = i;
+  url.forEach((item, index) => {
+    $(`<div class="maxPhoto-exhibit"><img class="maxPhoto-exhibit-img" src="${item}" alt=""></div>`)
+      .css({
+        left: (index - i) * 100 + 'vw'
+      })
+      .appendTo('.maxPhoto')
+  })
+}
+
+var next = function (e) {
+  return new Promise((resolve, reject) => {
+    // e.stopPropagation();
+    if (urlIndex < url.length - 1) {
+      ++urlIndex;
+      $.each($('.maxPhoto-exhibit'), function (index, n) {
+        $(n).css({
+          left: (index - urlIndex) * 100 + 'vw'
+        })
+      })
+    } else {
+      resolve(true)
+    }
+  })
+}
+
+
+var prev = function (e) {
+  return new Promise((resolve, reject) => {
+    // e.stopPropagation();
+    if (urlIndex > 0) {
+      --urlIndex;
+      $.each($('.maxPhoto-exhibit'), function (index, n) {
+        $(n).css({
+          left: (index - urlIndex) * 100 + 'vw'
+        })
+      })
+    } else {
+      resolve(true)
+    }
+  })
+}
+
+var prevX = function(e){
+  $('.maxPhoto').css({
+    bottom: '-100vh'
+  })
+  $('.maxPhoto-exhibit').remove();
+}
--- component/photo/photo.js
+++ component/photo/photo.js
@@ -1,64 +1,94 @@
-var photoLand = function (position) {
+var exhibit = false,
+  url = [],
+  urlIndex;
+
+var photoLand = function (position, fun,notifyFun) {
   var i = 0;
   var tiem = setInterval(() => {
 
     if (i < position.length) {
-      var photoBox = $(` <div class="photo-box">
+      var photoBox = $(` <div class="photo-box" >
       <img class="photo-img" src="${position[i].url}" alt="">
     </div>`).css({
-          top: 0,
+          top: -($('body').innerHeight() - $('.bg-christmas-tree').innerHeight()) + 'px',
           left: position[i].left,
           transition: `${position[i].speed}s linear`,
           transform: `rotate(${(Math.random() * -90) + 45 + 'deg'})`,
           // ...style
         })
         .appendTo('.bg-christmas-tree')
-      photo(position[i].text, photoBox, position[i].direction)
+
+      photo(position, photoBox, i, fun,notifyFun)
+      photoBox.attr({
+        'exhibit': 'false'
+      })
       photoBox.css({
         top: position[i].top,
       })
+
+      url.push(position[i].url)
+
       i++
     } else {
       clearInterval(tiem)
     }
 
-  }, 1000)
+  }, 500)
 
 }
 
 
 
-var photo = function (text, photoBox, direction) {
-  var bubble = $(`<span class="bubble">${text}</span>`).appendTo(photoBox)
+var photo = function (position, photoBox, i, fun,notifyFun) {
+  var bubble = $(`<span class="bubble">${position[i].text}</span>`).appendTo(photoBox)
   var width = bubble.outerWidth()
   bubble.css({
     width: '0',
     transition: '1s',
-    ...direction
+    ...position[i].direction
   })
   photoBox.click(function (e) {
-    
-    showBubble()
-    e.stopPropagation();
-    photoBox.css({
-      zIndex: 10,
-    })
-    bubble.css({
-      width: width + 10 + 'px',
-      padding: '5px',
-    });
+    if (exhibit && $(e.currentTarget).attr('exhibit') == 'true') {
+      $('.maxPhoto').css({
+        bottom: 0
+      })
+      exhibit = false;
 
-    
+      fun(i)
+      notifyFun('点击退出，长按保存',{}, 4)
+      showBubble(photoBox)
+    } else {
+      showBubble(photoBox)
+
+
+      $(e.currentTarget).attr('exhibit', 'true')
+      e.stopPropagation();
+      photoBox.css({
+        zIndex: 10,
+      })
+      bubble.css({
+        width: width + 10 + 'px',
+        padding: '5px',
+      });
+      exhibit = true
+    }
+
+
   })
 
 }
 
 
-var showBubble = function () {
+var showBubble = function (photoBox) {
   $('.bubble').css({
     width: '0',
     padding: '5px 0',
   })
+  exhibit = false;
+
+  if (photoBox) {
+    $('.photo-box').attr('exhibit', 'false')
+  }
 
   $('.photo-box').css({
     zIndex: 3
